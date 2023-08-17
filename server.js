c482be1ae64e7eab48ce1ed290312daaa65bf978
Moroka <mmopanemoroka@gmail.com>
2023-08-17 22:31:23 +0100

    routes, mongoose, connect to mongoDB database

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
 // Routes
+app.get("/", (req, res) => {
+  res.send("Hello NODE API");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello NODE API')
-})
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog, My name is Kamogelo");
+});
 
-
-app.listen(3000, () => {
-    console.log(`Node API app is running on port 3000`)
-})
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(
+    "mongodb+srv://admin:Godkamo333@cluster0.mwssxlw.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`Node API app is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
