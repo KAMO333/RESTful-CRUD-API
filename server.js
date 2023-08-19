b4e4c5baa0116a2c94fae7822aff1183b14b4f18
Moroka <mmopanemoroka@gmail.com>
2023-08-19 17:36:50 +0100

    connected backend to front end with cors

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const productRoute = require("./routes/productRoute");
-const errorMiddleware = require('./middleware/errorMiddleware')
+const errorMiddleware = require("./middleware/errorMiddleware");
+var cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
+const FRONTEND = process.env.FRONTEND
 
+var corsOptions = {
+  origin: FRONTEND,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -24,7 +32,7 @@ app.get("/blog", (req, res) => {
   res.send("Hello Blog, My name is Kamogelo");
 });
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 mongoose.set("strictQuery", false);
 mongoose
